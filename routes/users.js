d5e56eea6832ef1f0588e906ea89ea9175cf1b09
Bruno Jerić <Brunojeric@gmail.com>
2021-11-08 08:34:27 +0100

    user5

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,7 @@ var router = express.Router();
 
 /* GET user profile. */
 router.get('/user', secured(), function (req, res, next) {
-  console.log(req.user);
   const { _raw, _json, ...userProfile } = req.user;
-  console.log('userProfile: ', userProfile);
   res.render('user', {
     userProfile: JSON.stringify(userProfile, null, 2),
     title: 'Profile page',
